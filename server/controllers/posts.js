17c53f40f1e6a2cd940fd891014957c5c5f7f111
Steven Velez <stevendvelez9@gmail.com>
2020-12-13 16:46:09 -0500

    finished project

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,9 +2,17 @@ import mongoose from "mongoose";
 
 import PostMessage from "../models/postMessage.js";
 
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+        res.status(200).json(postMessages);
+    } catch(err) {
+        res.status(404).json({message: err.message});
+    }
+}
+
 export const createPost = async (req, res) => {
     const post = req.body;
-    console.log(post);
     const newPost = new PostMessage(post);
 
     try {
@@ -12,15 +20,6 @@ export const createPost = async (req, res) => {
         res.status(201).json(newPost);
     } catch(err) {
         res.status(409).json({message: err.message});
-    }
-}
-
-export const getPosts = async (req, res) => {
-    try {
-        const postMessages = await PostMessage.find();
-        res.status(200).json(postMessages);
-    } catch(err) {
-        res.status(404).json({message: err.message});
     }
 }
 
@@ -33,6 +32,31 @@ export const updatePost = async (req, res) => {
     }
 
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new: true});
+
+    res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const {id} = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send("No post with that id");
+    }
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({message: "Post deleted successfully"});
+}
+
+export const likePost = async (req, res) => {
+    const {id} = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send("No post with that id");
+    }
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount + 1}, {new: true});
 
     res.json(updatedPost);
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -13,10 +15,13 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://runandJUMP:BMnWFOsoMwTSzRHZ@depoppler.xznul.mongodb.net/MERNProject?retryWrites=true&w=majority";
+app.get("/", (req, res) => {
+    res.send("Hello to Memories API");
+});
+
 const port = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(port, () => console.log(`Server funning on port: ${port}`)))
     .catch((err) => console.log(err.message));
 
